2d317bbcd72ad57fe7d1969e0197ac3e236459aa
rodrigo4244 <rodrigo4244@users.noreply.github.com>
2017-07-25 15:51:12 -0300

    refactor: use ternary operators instead of conditional

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const assert = require('assert')
 const fs = require('fs')
 const path = require('path')
+
 const {
   assign,
   flow,
@@ -43,19 +44,10 @@ const parsers = ({ peg = true, pegimport = false, pathdir = `${__dirname}/test/p
   const filesArray = listPegjsFiles([pathdir])
 
   return filesArray.reduce((acc, file) => {
-    if (pegimport) {
-      const name = path.parse(file).name
-      const buildParser = partialRight(peg.buildParser)([pegOptions])
-      const gracefulParser = flow(buildParser, makeGraceful)(file)
-      const nonGracefulParser = buildParser(file).parse
-      const parser = (graceful) ? gracefulParser : nonGracefulParser
-      return assign(acc)({ [name]: parser })
-    }
-
     const name = path.parse(file).name
-    const buildParser = partialRight(peg.generate)([pegOptions])
-    const gracefulParser = flow(turnFileIntoGrammar, buildParser, makeGraceful)(file)
-    const nonGracefulParser = flow(turnFileIntoGrammar, buildParser)(file).parse
+    const buildParser = (pegimport) ? partialRight(peg.buildParser)([pegOptions]) : partialRight(peg.generate)([pegOptions])
+    const gracefulParser = (pegimport) ? flow(buildParser, makeGraceful)(file) : flow(turnFileIntoGrammar, buildParser, makeGraceful)(file)
+    const nonGracefulParser = (pegimport) ? buildParser(file).parse : flow(turnFileIntoGrammar, buildParser)(file).parse
     const parser = (graceful) ? gracefulParser : nonGracefulParser
     return assign(acc)({ [name]: parser })
   }, {})
