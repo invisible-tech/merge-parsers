ba2ce825a2e498e148fcc30ef0462d4c92238492
Keenahn Tiberius Jung <keenahn@users.noreply.github.com>
2018-08-20 09:22:19 -0700

    style: eslint

--- test/index.spec.js
+++ test/index.spec.js
@@ -7,7 +7,7 @@ const {
   isEmpty,
 } = require('lodash/fp')
 
-const parsers = require('../')
+const parsers = require('../') // eslint-disable-line unicorn/import-index
 
 describe('parsers', () => {
   it('should return the filenames as keys', () => {
